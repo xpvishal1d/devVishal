dabb515b7e74aae5f12d698017bfce6260ac17bc
Vishal Singh <151445482+xpvishal1d@users.noreply.github.com>
2024-02-19 15:41:53 +0000

    opraters conversion

---
+++ muskan/5thdatatypeconversionandopration.js
@@ -0,0 +1,19 @@
+let value = 4
+let negValue = - value
+
+//console.log(negValue);
+
+let str1 = "vishal "
+let str2 = "singh"
+let str3 = str1 +str2
+console.log([str3,negValue]);
+
+console.log(2+"2");
+console.log("2"+"2");
+console.log(3+2+"2");
+console.log("5"+2+2); // भाई यहाँ पैर क्या हो रहा है ??
+// किसको प्राथमिकता मिल रही है देख के 
+
+
+
+
