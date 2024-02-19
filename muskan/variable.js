4b7b11621b3ab4e273d3616218fc8cf44b38474f
Vishal Singh <151445482+xpvishal1d@users.noreply.github.com>
2024-02-19 07:23:09 +0000

    variable

---
+++ muskan/variable.js
@@ -0,0 +1,16 @@
+const accountId = 123654
+let  accountEmail ="vishal1252@gamil.com"
+var accountPassword = "12345viahal"
+accountCity = "kushinagar"
+let state;
+
+//accountId = 2
+accountEmail ="vishal1252@xp.com"
+accountPassword = "1viahal"
+accountCity = "kushi"
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,state, ]);
+
+
