6c7de01c551b979cb0a29bd195610b1ab46ec3be
Vishal Singh <visi125220@gmail.com>
2024-02-20 00:11:26 +0530

    9th and 8th video

--- muskan/5thdatatypeconversionandopration.js
+++ muskan/5thdatatypeconversionandopration.js
@@ -3,16 +3,19 @@ let negValue = - value
 
 //console.log(negValue);
 
-let str1 = "vishal "
-let str2 = "singh"
-let str3 = str1 +str2
-console.log([str3,negValue]);
+// let str1 = "vishal "
+// let str2 = "singh"
+// let str3 = str1 +str2
+// console.log([str3,negValue]);
 
-console.log(2+"2");
-console.log("2"+"2");
-console.log(3+2+"2");
-console.log("5"+2+2); // भाई यहाँ पैर क्या हो रहा है ??
+// console.log(2+"2");
+// console.log("2"+"2");
+// console.log(3+2+"2");
+// console.log("5"+2+2); // भाई यहाँ पैर क्या हो रहा है ??
 // किसको प्राथमिकता मिल रही है देख के 
+
+// console.log(+true);
+
 
 
 
---
+++ muskan/6thcom.js
@@ -0,0 +1,19 @@
+/*
+console.log(2<1);
+console.log(2>1);
+console.log(2>=1);
+console.log(2==1);
+console.log(2!=1);
+
+// symple
+*/
+// console.log("2">1); ये "2" को नंबर में  बदल दिया है 
+
+/*
+console.log(null > 0);
+console.log(null < 0);
+console.log(null >= 0);
+console.log(null == 0);
+console.log(null != 0);
+// यहाँ देखो बड़ा नहीं है लेकिन बराबर से थोडा बड़ा है 
+*/
