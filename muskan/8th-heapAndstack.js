5faea477b62f26d74929b14d0affdf1d768eed19
Vishal Singh <visi125220@gmail.com>
2024-02-20 11:49:23 +0530

    change in data type

--- muskan/8th-heapAndstack.js
+++ muskan/8th-heapAndstack.js
@@ -3,3 +3,19 @@ let userName ="mohan"
   
   anotherName = "ram"
 console.log(userName);
+console.log(anotherName);
+
+let userOne = {
+
+  email: "xp@.com",
+  upi : " phonepe@ybl"
+
+}
+
+let userTwo = userOne
+
+userTwo.email = "vishal@.com"
+
+console.log(userOne.email);
+console.log(userTwo.email);
+
