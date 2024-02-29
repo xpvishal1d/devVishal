cc4ce93205590ef23b26aedd22cbbf9e6afe5c50
Vishal Singh <visi125220@gmail.com>
2024-02-29 11:14:56 +0530

    restart

---
+++ fase-2muskan/4objects.js
@@ -0,0 +1,63 @@
+
+
+const tinderUser = new Object()
+console.log(tinderUser);
+
+ tinderUser ={}
+
+
+tinderUser.id="123abc"
+tinderUser.name = "same"
+tinderUser.isLogedIn = false
+
+// // console.log(tinderUser);
+
+// const regularUser = {
+//     email : "some@gmail.com",
+//     fullName :{
+//         userFullName:{
+//             firstName : "vishal",
+//             lastName: "singh"
+//         }
+//     }
+// }
+
+// console.log(regularUser.fullName);
+
+const obj1 = { 1: "a" , 2 :"b"}
+const obj2 = { 3: "c" , 4 :"d"}
+const obj4 = { 4:"a" , 5 :"g"}
+
+
+// const obj3 = {obj1, obj2};
+// const obj3 = Object.assign({}, obj1,obj2,obj4) 
+// const obj3= {...obj1, ...obj2}
+// console.log(obj3);
+
+// server se data eise aata hai
+const users =[
+    {
+        id: 1,
+        emailId :"h@gamil.com",
+
+
+    },
+    {
+
+    },
+    {
+
+    },
+]
+
+users[1].emailId;
+
+
+
+
+
+
+
+
+
+
