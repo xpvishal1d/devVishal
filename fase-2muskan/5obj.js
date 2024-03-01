d74f96cd3f6605903eab703af9bbc640bf1409fc
Vishal Singh <visi125220@gmail.com>
2024-03-01 22:06:17 +0530

    obj complete json start

--- fase-2muskan/5obj.js
+++ fase-2muskan/5obj.js
@@ -25,8 +25,35 @@ const regularUser = {
 
 const obj1 = { 1: "a", 2: "b"} 
 const obj2 = { 3: "c", 4: "d"} 
+const obj4 = {  5 : "e" , 6 : "f" }
 
-const obj3 = { obj1,  obj2 }
+// const obj3 = { obj1,  obj2 }
+// const obj3 =Object.assign({}, obj1, obj2 , obj4 )
 
-console.log(obj3);
+const obj3 = {...obj1 ,...obj2 , ...obj4}
 
+// console.log(obj3);
+
+const users = [
+    {
+
+    }
+]
+
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+// console.log(tinderUser.hasOwnProperty('isLogedIn'));
+
+const course ={
+    courseName : "Js pathshala",
+    price :" $99" ,
+    courseInstructor : "hitesh"
+}
+
+// console.log( course.courseInstructor);
+
+const {courseInstructor : Instructor}= course
+
+console.log(Instructor);
