516f547fc2800ffbe7f289676558c5b13a383ea9
Vishal Singh <visi125220@gmail.com>
2024-03-05 22:47:16 +0530

    function scope

---
+++ fase-3muskan/02scopes.js
@@ -0,0 +1,48 @@
+// let a = 10
+// const b = 20
+// var c = 30
+// d =40
+
+// console.log(a ,b ,c ,d);
+
+
+// function one (){
+//     const username = " nikal"
+
+// function two(){
+//     const website = "instagram"
+//     console.log(username);
+// }
+// // console.log(website);
+// two()
+// }
+// one ()
+
+if (true) {
+    const  username = "nikal"
+    if (username=== "nikal") {
+        const website = " instagram"
+
+        console.log(username + website);
+        
+    }
+    // console.log(website);
+    
+}
+
+// console.log(username);
+
+// +++++++++++++++++++interesting++++++++++++++++++++++
+
+console.log(addOne(9))
+
+function addOne (num){
+return num + 1
+}
+
+
+const addTwo = function (num){
+    return num +2
+}
+
+addTwo(9)
